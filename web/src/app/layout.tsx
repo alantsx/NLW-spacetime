26cb997a3a6059f09f719c67ffec505b7b7e6a8d
Alan <alanpfabricio@gmail.com>
2023-06-01 22:40:32 -0300

    memory listing web

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
           </div>
 
           {/* Right */}
-          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex overflow-y-scroll max-h-screen flex-col bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
